b90475cbff2a33d318e754568cf809725d443430
rahult017 <rahult016@gmail.com>
2024-12-15 14:52:53 +0530

    change done in all views and requirements

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-python manage.py collectstatic --noinput
-
-gunicorn Event_Management_With_Swaggerr.wsgi:application --workers 8 \
-    --bind 0.0.0.0:8000 \
-    --log-file /usr/src/app/logs/gunicorn/gunicorn.log \
-    --access-logfile /usr/src/app/logs/gunicorn/access.log \
-    --error-logfile /usr/src/app/logs/gunicorn/error.log
